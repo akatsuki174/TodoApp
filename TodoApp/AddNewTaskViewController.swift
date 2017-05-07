abfc111691a8784465a88aebd9e01d7ae2f7ba4f
akatsuki174 <dev2015js@gmail.com>
2017-05-07 20:37:51 +0900

    issue #11 おおまかなUIの作成

---
+++ TodoApp/AddNewTaskViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddNewTaskViewController.swift
+//  TodoApp
+//
+//  Created by akatsuki174 on 2017/05/07.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class AddNewTaskViewController : UIViewController {
+    
+    @IBOutlet weak var memoTextView: UITextView!
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        memoTextView.layer.borderColor = UIColor(red: 229/255.0, green: 229/255.0, blue: 229/255.0, alpha: 1.0).cgColor
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func tappedCloseBtn(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tappedAddBtn(_ sender: Any) {
+        // TODO: タスク追加処理
+        
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+}
