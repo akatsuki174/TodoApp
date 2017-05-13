9c34c45c266671bedcb72022c1f8270f0e141acf
akatsuki174 <dev2015js@gmail.com>
2017-05-13 23:11:07 +0900

    issue #10 オプショナルで返すように修正

--- TodoApp/DateUtils.swift
+++ TodoApp/DateUtils.swift
@@ -12,11 +12,11 @@ class DateUtils {
     static let locale = Locale(identifier: "ja_JP")
     static let dateFormatter = "yyyy/MM/dd"
     
-    class func dateFromString(string: String) -> Date {
+    class func dateFromString(string: String) -> Date? {
         let formatter: DateFormatter = DateFormatter()
         formatter.locale = locale
         formatter.dateFormat = dateFormatter
-        return formatter.date(from: string)!
+        return formatter.date(from: string)
     }
     
     class func stringFromDate(date: Date) -> String {
