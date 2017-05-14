2027e86826a5d3afbfb94512f138a2829f5ef876
akatsuki174 <dev2015js@gmail.com>
2017-05-14 14:55:38 +0900

    issue #16 ローカルプッシュをお試し実装

--- TodoApp/AppDelegate.swift
+++ TodoApp/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let settings = UIUserNotificationSettings(types: [.badge, .alert], categories: nil)
+        application.registerUserNotificationSettings(settings)
+        application.registerForRemoteNotifications()
         return true
     }
 
@@ -25,8 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let notification = UILocalNotification()
+        notification.alertBody = "テスト"
+        notification.fireDate = Date(timeIntervalSinceNow:3) // TODO: あとで直す
+        notification.applicationIconBadgeNumber += 1
+        notification.userInfo = ["id": 1] // TODO: あとで直す
+        application.scheduleLocalNotification(notification)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -34,13 +40,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        application.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+        if application.applicationState != .active {
+            application.applicationIconBadgeNumber = 0
+            application.cancelLocalNotification(notification)
+        } else {
+            if application.applicationIconBadgeNumber != 0 {
+                application.applicationIconBadgeNumber = 0
+                application.cancelLocalNotification(notification)
+            }
+        }
+    }
 }
 
