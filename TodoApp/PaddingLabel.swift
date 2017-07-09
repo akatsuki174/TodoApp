d17fda19779b0440f99e784b66dc1a9ac89fd4b3
akatsuki174 <dev2015js@gmail.com>
2017-07-09 17:31:13 +0900

    issue #25 期限ラベルのpaddingを調整

---
+++ TodoApp/PaddingLabel.swift
@@ -0,0 +1,26 @@
+//
+//  PaddingLabel.swift
+//  TodoApp
+//
+//  Created by akatsuki174 on 2017/07/09.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+    
+    var insets = UIEdgeInsetsMake(0, 0, 0, 0)
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += insets.top + insets.bottom
+        contentSize.width += insets.left + insets.right
+        return contentSize
+    }
+    
+}
--- TodoApp/TaskListCell.swift
+++ TodoApp/TaskListCell.swift
@@ -11,10 +11,11 @@ import UIKit
 class TaskListCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var limitLabel: UILabel!
+    @IBOutlet weak var limitLabel: PaddingLabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        limitLabel.insets = UIEdgeInsetsMake(3, 3, 3, 3)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
